f6188796b06c57c588a7d7ed21d61fc929d9408a
bradyliles <bradyliles@outlook.com>
2014-11-04 23:54:22 -0700

    Initial Commit

---
+++ Controllers/CoursesController.cs
@@ -0,0 +1,38 @@
+﻿using System.Web.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace Angular4DotNetMVC.Controllers
+{
+    public class CoursesController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View("Index", "", GetSerializedCourseVms());
+        }
+
+        public string GetSerializedCourseVms()
+        {
+            var courses = new[]
+            {
+                new CourseVm {Number = "CS101", Name = "Course Name 1", Instructor = "instructor1"},
+                new CourseVm {Number = "CS202", Name = "Course Name 2", Instructor = "instructor2"},
+                new CourseVm {Number = "CS303", Name = "Course Name 3", Instructor = "instructor3"}
+            };
+            var settings = new JsonSerializerSettings {ContractResolver = new CamelCasePropertyNamesContractResolver()};
+            return JsonConvert.SerializeObject(courses, Formatting.None, settings);
+        }
+
+    }
+
+
+
+    public class CourseVm
+    {
+        public string Number { get; set; }
+        public string Name { get; set; }
+        public string Instructor { get; set; }
+    }
+}
+
+
