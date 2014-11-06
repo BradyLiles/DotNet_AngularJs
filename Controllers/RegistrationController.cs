168cef2ab1a6be6903d7e6b9f5b5934f7b50665c
bradyliles <bradyliles@outlook.com>
2014-11-05 17:32:04 -0700

    MiniSpa Part 1

---
+++ Controllers/RegistrationController.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Mvc;
+using Angular4DotNetMVC.Models.Registration;
+
+namespace Angular4DotNetMVC.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        private RegistrationVmBuilder _registrationVmBuilder = new RegistrationVmBuilder();
+
+        public ActionResult Index()
+        {
+            return View(_registrationVmBuilder.BuilRegistrationVm());
+        }
+
+    }
+}
